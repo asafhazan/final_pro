623dfcb302ce28b3ca7158777e95b3a3c61f4b2a
asafhz <42414105+asafhz@users.noreply.github.com>
2019-02-20 17:43:45 +0200

    Update Rest.py

--- venv/Rest.py
+++ venv/Rest.py
@@ -1,62 +1,76 @@
-from flask import Flask, request , jsonify
+from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
-import json
-import urllib.request
-
+import requests
 
 app = Flask(__name__)
 api = Api(app)
 
 camera = {
     "speed": "1000",
-    "f": "2.8",
-    "iso": "1800",
-    "ldr": "0"
+    "f": "4.5",
+    "iso": "1800"
 }
+lightMin = 0
+lightMax = 800
+
+minShater = 350
+maxShater = 2000
+
 
 arduino = {
-    "Water_Drop": "1"
+    "light_sensor": "400",
 }
 
+android = {
+"Water_Drop": "10"
+}
 
+light_sensor = {}
 
-print(camera)
+def generateParams(light_sensor):
+    light = arduino["light_sensor"]
+    lightPersentage = (float(light) * 100) / float(lightMax) # float
+    diff_Max_Min = maxShater - minShater # 1650, int
+    revercePercentage = lightPersentage * 0.01 # 50 --> 0.5
+    shaterCalc = diff_Max_Min * revercePercentage
+    camera['speed'] = shaterCalc
+
 
 class Camera(Resource):
-    def get(self, speed, f, iso):
-        if camera[speed] is not None and camera[f] is not None and camera[iso] is not None:
-            return jsonify(camera)
+    def post(self):
+        all_args = request.json
+        print(all_args)
 
-    # def get(self, speed):
-    #     return camera["speed"]
+    def put(self):
+        light_sensor = request.args.get('aaa')
+        print(light_sensor)
 
-    # def put(self):
-    #     data = request.get_json()
-    #     camera["speed"]:(data)
-    #     return camera
-
-    def put(self,speed):
-        data = request.get_json()
-        # camera.update(data)
-        # camera["speed"] = speed
-        return data
+    def get(self):
+        generateParams(arduino['light_sensor'])
+        return jsonify(camera)
 
 class Arduino(Resource):
-    def get(self,Water_Drop):
-        return jsonify (arduino)
-
-    def put(self, Water_Drop):
-        data1 = request.get_json()
-        arduino.update(data1)
-        # camera["speed"] = speed
-        return jsonify(arduino)
+    def post(self):
+        light_sensor = request.json
+        light_sensor = light_sensor['light_sensor']
+        arduino['light_sensor'] = light_sensor
+        print(arduino['light_sensor'])
 
 
+class Android(Resource):
+    def put(self):
+        water = request.json
+        android.update(water)
+        print(android)
+        return jsonify(android)
 
-api.add_resource(Camera,'/Camera/<string:speed>/<string:f>/<string:iso>')
-# api.add_resource(light,'/light/<string:l>')
-api.add_resource(Arduino,'/Arduino/<string:Water_Drop>')
+    def get(self):
+        print(android)
+        return jsonify(android)
 
 
+api.add_resource(Arduino, '/arduino')
+api.add_resource(Android,'/android/')
+api.add_resource(Camera, '/camera')
 
-app.run(host='192.168.50.22', port=5000)
+app.run(host='192.168.10.31', port=5000)
